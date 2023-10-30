72111a1a6d3fc8c07360a357928adebbc18ae1f4
lathiyamilan5 <lathiyamilan5@gmail.com>
2023-10-30 17:20:03 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserlistComponent } from './user-management/userlist.component';
+
+
+const routes: Routes = [
+  {
+    path : '', redirectTo : 'users', pathMatch : 'full'
+  },
+  {
+    path : 'users', component : UserlistComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
